336247f83f47d3be2d413df14b6d23185b7f9e5a
Yuvraj Singh <singh.yuvraj1047@gmail.com>
2021-04-14 18:41:01 +0530

    basic project setup

---
+++ public/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC9ls5vN8fGrygfbFu5evmaR0QY9334Tt8",
+    authDomain: "feedback-system-ggv.firebaseapp.com",
+    projectId: "feedback-system-ggv",
+    storageBucket: "feedback-system-ggv.appspot.com",
+    messagingSenderId: "691921438408",
+    appId: "1:691921438408:web:6f7c77315c500e22f047fd",
+    measurementId: "G-NP4RH0Q984"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
